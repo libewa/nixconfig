566e832799d8107d09b6d681fe16c730bd5d416b
libewa <libewa.git@icloud.com>
2024-05-04 01:58:13 +0200

    local home it is after all

--- hosts/yoga/home-manager.nix
+++ hosts/yoga/home-manager.nix
@@ -2,11 +2,10 @@
 
 {
   home-manager = {
+    backupFileExtension = "bak";
     extraSpecialArgs = { inherit inputs; };
     users = {
-      "linus" = pkgs.fetchgit {
-        url = "https://gist.github.com/3169f2fc02aa56a78bae1cdf2231078d.git";
-      }
+      linus = import ../../homes/linus.nix;
     };
   };
 }
