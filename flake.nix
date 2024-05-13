19f3bfc599b8813f640ef5df99746fb9799b047e
libewa <libewa.git@icloud.com>
2024-05-13 16:39:26 +0200

    remove home-manager from flake

--- flake.nix
+++ flake.nix
@@ -6,9 +6,6 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     cdpkgs.url = "nixpkgs/23.11";
 
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs/23.11";
-
     livecdhome.url = "github:libewa/home-manager";
   };
 
