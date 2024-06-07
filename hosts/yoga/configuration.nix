a9329c472d24fd0c093381b00c64910c6847c7a2
libewa <libewa.git@icloud.com>
2024-06-07 17:13:21 +0200

    use kde instead of gnome

--- hosts/yoga/configuration.nix
+++ hosts/yoga/configuration.nix
@@ -50,9 +50,8 @@
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
-  # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.desktopManager.plasma6.enable = true;
 
   services.logind.powerKey = "hibernate";
   services.logind.powerKeyLongPress = "poweroff";
