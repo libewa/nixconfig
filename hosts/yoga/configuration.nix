271d445dd55b071ce984df442b61757ac545e2ba
libewa <libewa.git@icloud.com>
2024-06-08 21:31:59 +0200

    go back to gdm

--- hosts/yoga/configuration.nix
+++ hosts/yoga/configuration.nix
@@ -50,7 +50,7 @@
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
-  services.displayManager.sddm.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
 
   services.logind.powerKey = "hibernate";
