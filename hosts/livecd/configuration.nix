e1400da62c29f7549d1c7962f425300401484c7d
libewa <libewa.git@icloud.com>
2024-06-13 20:57:43 +0200

    remove unnecessary nix channel alias (flakes r cool)

--- hosts/livecd/configuration.nix
+++ hosts/livecd/configuration.nix
@@ -11,13 +11,6 @@
   nixpkgs.config = {
     # Allow proprietary packages
     allowUnfree = true;
-
-    # Create an alias for the unstable channel
-    #packageOverrides = pkgs: {
-    #  unstable = import <nixos-unstable> { # pass the nixpkgs config to the unstable alias # to ensure `allowUnfree = true;` is propagated:
-    #    config = config.nixpkgs.config;
-    #  };
-    #};
   };
 
   system.autoUpgrade.enable = false;
--- hosts/yoga/configuration.nix
+++ hosts/yoga/configuration.nix
@@ -13,13 +13,6 @@
   nixpkgs.config = {
     # Allow proprietary packages
     allowUnfree = true;
-
-    # Create an alias for the unstable channel
-    #packageOverrides = pkgs: {
-    #  unstable = import <nixos-unstable> { # pass the nixpkgs config to the unstable alias # to ensure `allowUnfree = true;` is propagated:
-    #    config = config.nixpkgs.config;
-    #  };
-    #};
   };
 
   system.autoUpgrade.enable = true;
