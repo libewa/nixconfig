b5701775c4b91557177792cb6490124bc410ab02
libewa <libewa.git@icloud.com>
2024-06-23 13:47:43 +0200

    remove gnome

--- hosts/yoga/configuration.nix
+++ hosts/yoga/configuration.nix
@@ -42,7 +42,7 @@
 
   # Enable the X11 windowing system.
   services.xserver.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  #services.xserver.desktopManager.gnome.enable = true;
   services.gnome.gnome-keyring.enable = true;
 
   # Enable CUPS to print documents.
