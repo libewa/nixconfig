58045bd0dd6299d98b36cbe4877894cd49ec33dd
libewa <libewa.git@icloud.com>
2024-06-23 09:11:11 +0200

    add more lsps

--- modules/nvim/lsp.nix
+++ modules/nvim/lsp.nix
@@ -4,6 +4,7 @@
     plugins = with pkgs.vimPlugins; [
       nvim-lspconfig
       cmp-nvim-lsp
+      luasnip
     ];
     extraPackages = with pkgs; [
       arduino-language-server
@@ -12,11 +13,19 @@
       libclang
       deno
       nixd
+      vscode-langservers-extracted
+      dprint
+      emmet-language-server
+      rust-analyzer
+      typescript
+      nodePackages.typescript-language-server
     ];
     extraLuaConfig = ''
     lsp = require('lspconfig')
     -- Set up lspconfig.
     local capabilities = require('cmp_nvim_lsp').default_capabilities()
+    capabilities.textDocument.completion.completionItem.snippetSupport = true
+
     -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
     lsp.nil_ls.setup {
       capabilities = capabilities
@@ -27,12 +36,52 @@
     lsp.sourcekit.setup {
       capabilities = capabilities
     }
+    vim.g.markdown_fenced_languages = {
+      "ts=typescript"
+    }
     lsp.denols.setup {
-      capabilities = capabilities
+      capabilities = capabilities,
+      settings = {
+        enable = true,
+	suggest = {
+	  imports = {
+	    hosts = {
+	      ["https://deno.land"] = true,
+	      ["https://jsr.io"] = true
+	    }
+	  }
+	}
+      }
     }
     lsp.arduino_language_server.setup {
       capabilities = capabilities
     }
+
+    lsp.cssls.setup {
+      capabilities = capabilities
+    }
+    lsp.jsonls.setup {
+      capabilities = capabilities
+    }
+    lsp.html.setup {
+      capabilities = capabilities
+    }
+    
+    lsp.dprint.setup {
+      capabilities = capabilities
+    }
+    lsp.emmet_language_server.setup {
+      capabilities = capabilities
+    }
+    lsp.rust_analyzer.setup {
+      capabilities = capabilities
+    }
+    lsp.tsserver.setup {
+      capabilities = capabilities
+    }
+
+    require("luasnip.loaders.from_vscode").lazy_load()
+    require("luasnip.loaders.from_snipmate").lazy_load()
     '';
   };
 }
