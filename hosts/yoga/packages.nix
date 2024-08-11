d547e73bcb8aedf4ff6fb80bc36bc2c150b2e1ce
libewa <libewa.git@icloud.com>
2024-08-11 21:53:54 +0200

    add nh

--- hosts/yoga/packages.nix
+++ hosts/yoga/packages.nix
@@ -24,6 +24,11 @@
     remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
     dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
   };
+  programs.nh = {
+    enable = true;
+    clean.enable = true;
+    flake = /home/linus/nixconfig;
+  };
   hardware.opengl.driSupport32Bit = true;
   programs.hyprland.enable = true;
   programs.nm-applet.enable = true;
