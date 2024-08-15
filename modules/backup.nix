eec61f6200b91537c3b20ea3843720384993b0f1
libewa <libewa.git@icloud.com>
2024-08-15 15:48:33 +0200

    use btrbk

--- modules/backup.nix
+++ modules/backup.nix
@@ -1,13 +1,19 @@
 {
-  services.borgbackup.jobs.home = { # Root backing each day up to a remote backup server. We assume that you have
-    #   * created a password less key: ssh-keygen -N "" -t ed25519 -f /path/to/ssh_key
-    #     best practices are: use -t ed25519, /path/to = /run/keys
-    paths = [ "/etc" "/home" ];
-    exclude = [ "/nix" "'**/.cache'" "'**/node_modules'" "'**/.venv'" "'**/venv'" ];
-    repo =  "ssh://borg-backup@nas:22/volume1/backup/yoga";
-    encryption.mode = "none";
-    environment = { BORG_RSH = "ssh -i /run/keys/borg/id_ed25519_borg"; };
-    compression = "auto,lzma";
-    startAt = "daily";
+  services.btrbk = {
+    instances.nas = {
+      onCalendar = "daily";
+      settings = {
+        ssh_identity = "/home/btrbk";
+	ssh_user = "backup";
+	stream_compress = "xz";
+	snapshot_preserve = "7d 4w 12m";
+	target_preserve = "7d 4w 12m";
+	snapshot_preserve_min = "7d";
+	volume."/btrfs-root" = {
+	  target = "ssh://nas/volume1/backup/yoga";
+	  subvolume = "home";
+	};
+      };
+    };
   };
 }
