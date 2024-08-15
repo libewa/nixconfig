d83e85b7bceaddf96d3e6ca8bc90e287d3e5387c
libewa <libewa.git@icloud.com>
2024-08-15 09:45:10 +0200

    fix zsh lang

--- modules/zsh.nix
+++ modules/zsh.nix
@@ -13,7 +13,7 @@
     alias ll='ls -l --color=auto'
     alias nrs='nixos-rebuild switch'
     alias hms='home-manager switch'
-    export LANG=en_US LANGUAGE=en_US LC_ALL=de_DE
+    export LANG=en_US LANGUAGE=en_US LC_ALL=de_DE.UTF-8
 
     fastfetch
     ";
