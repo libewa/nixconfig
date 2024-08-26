9015bdf826a698c302a035b77683766329330d08
libewa <libewa.git@icloud.com>
2024-08-26 15:07:37 +0000

    add wsl config

---
+++ hosts/wsl/packages.nix
@@ -0,0 +1,14 @@
+{ pkgs, ... }:
+{
+  environment.systemPackages = with pkgs; [
+    vscodium
+    pv
+    fastfetch
+  ];
+  programs.nh = {
+    enable = true;
+    clean.enable = true;
+    flake = "/home/nixos/nixconfig";
+  };
+  programs.zsh.enable = true;
+}
