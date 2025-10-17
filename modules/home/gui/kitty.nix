ac235cfd1c6bab0b96161d6359cfbe3f153a04aa
libewa <linus@libewa.xyz>
2025-10-17 16:26:54 +0200

    kitty rice and qat

--- modules/home/gui/kitty.nix
+++ modules/home/gui/kitty.nix
@@ -5,5 +5,14 @@
       package = pkgs.cascadia-code;
       name = "Cascadia Code NF";
     };
+    enableGitIntegration = true;
+    settings = {
+      cursor_trail = 5;
+      scrollbar_interactive = true;
+      remember_window_size = false;
+    };
+    quickAccessTerminalConfig = {
+      start_as_hidden = true;
+    };
   };
 }
