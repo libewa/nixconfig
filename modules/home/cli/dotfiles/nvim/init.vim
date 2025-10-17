4b840669262d5f71e3550221bf43bd0e75cea0b9
libewa <linus@libewa.xyz>
2025-10-17 16:26:43 +0200

    proper neovim indents

--- modules/home/cli/dotfiles/nvim/init.vim
+++ modules/home/cli/dotfiles/nvim/init.vim
@@ -6,4 +6,9 @@ set foldmethod=indent
 set foldlevelstart=99
 set lcs=tab:>->,lead:·,trail:·,extends:>,precedes:<,nbsp:¦
 
+set shiftwidth=2 smarttab expandtab
+set softtabstop=0
+set tabstop=8
+set autoindent
+
 tnoremap <Esc> <C-\><C-n>
--- modules/home/cli/nvim/lsp.nix
+++ modules/home/cli/nvim/lsp.nix
@@ -1,29 +1,29 @@
 {pkgs, ...}: {
   programs.nixvim = {
     plugins.lsp = {
-        enable = true;
-        #TODO: snippets
-        servers = {
-          nil_ls.enable = true;
-          nixd.enable = true;
-          sourcekit.enable = true;
-          denols = {
-            enable = true;
-            settings.suggest.imports.hosts = [
-              "https://deno.land"
-              "https://jsr.io"
-            ];
-          };
-          arduino_language_server.enable = true;
-          cssls.enable = true;
-          jsonls.enable = true;
-          html.enable = true;
-          clangd.enable = true;
-          dprint.enable = true;
-          emmet_language_server.enable = true;
-          #rust_analyzer.enable = true;
+      enable = true;
+      #TODO: snippets
+      servers = {
+        nil_ls.enable = true;
+        nixd.enable = true;
+        #sourcekit.enable = true;
+        denols = {
+          enable = true;
+          settings.suggest.imports.hosts = [
+            "https://deno.land"
+            "https://jsr.io"
+          ];
         };
+        arduino_language_server.enable = true;
+        cssls.enable = true;
+        jsonls.enable = true;
+        html.enable = true;
+        clangd.enable = true;
+        dprint.enable = true;
+        emmet_language_server.enable = true;
+        #rust_analyzer.enable = true;
       };
+    };
     extraConfigLua = /*lua*/ ''
     vim.g.markdown_fenced_languages = {
       "ts=typescript"
