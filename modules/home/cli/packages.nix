8d3b1eb42a20e34a2eb7b8902879a70f4fe9dda1
Linus <linus@libewa.xyz>
2025-11-17 19:28:01 +0100

    remove home packages provided by host os

--- modules/home/cli/packages.nix
+++ modules/home/cli/packages.nix
@@ -1,17 +1,6 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [
-    tree
-    gh
-    tea
-    audacity
-    lynx
-    cmatrix
-    nil
-    ffmpeg
     lazygit
-    rustup
-    deno
-    arduino-cli
     nix-search-cli
   ];
 }
